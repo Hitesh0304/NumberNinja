a4e6b267596c3db10da1ccf83d755cf8065749d4
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2023-11-08 13:07:02 -0500

    added activities

--- app/src/main/java/com/hm/numberninja/MainActivity.kt
+++ app/src/main/java/com/hm/numberninja/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.hm.numberninja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var addition: Button
+    lateinit var subtraction: Button
+    lateinit var multiplication: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addition = findViewById(R.id.addButton)
+        subtraction = findViewById(R.id.subtractionButton)
+        multiplication = findViewById(R.id.multiplicationButton)
+
+        addition.setOnClickListener {
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
