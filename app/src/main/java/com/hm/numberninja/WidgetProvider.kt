3c3d9f4ef3beae12171e6452b5be296c164a0aaa
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2023-12-04 00:42:46 -0500

    added widget

---
+++ app/src/main/java/com/hm/numberninja/WidgetProvider.kt
@@ -0,0 +1,41 @@
+package com.hm.numberninja
+
+import android.appwidget.AppWidgetProvider
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.widget.RemoteViews
+
+class WidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform operations when the widget is updated
+
+        // Loop through all widgets
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    companion object {
+        internal fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            // Create an instance of RemoteViews and set its layout
+            val views = RemoteViews(context.packageName, R.layout.widget_layout)
+
+            // Set some text on the widget
+            views.setTextViewText(R.id.widgetTextView, "Number Ninja")
+
+            views.setImageViewResource(R.id.widgetImageView, R.drawable.numbers)
+
+            // Update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
